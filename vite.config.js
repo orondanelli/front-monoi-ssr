1174b5037bdaa61e3e186e700f4d6076f2dfd8db
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-05 11:34:59 -0400

    fixing idk

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import viteSSR from 'vite-ssr/plugin.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteSSR()],
+  devServer: {
+    proxy: 'http://api-monoi.herokuapp.com',
+},
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
