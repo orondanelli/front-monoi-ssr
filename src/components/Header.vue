be2f76ba0afbebc0253a54a8bab39273c65e343f
orlando rondanelli <orlando.rondanelli@gmail.com>
2022-04-08 00:47:08 -0400

    :sparkles: show time ago from scrapped data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,39 @@
 <template>
-<div class="header">
-    <span class="center-text">Monoi barato</span>
-</div>
+    <div class="header">
+        <span class="center-text">Monoi barato </span>
+        <span class="updated-text">(Actualizado {{ textTimeAgo }})</span>
+    </div>
 </template>
+
+<script>
+
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            lastUpdated: "",
+            textTimeAgo: "",
+            loading: true,
+            api_url: "http://api-monoi.herokuapp.com/products/lastUpdate",
+        }
+    },
+    created() {
+        axios
+            .get(this.api_url)
+            .then(response => { 
+                this.lastUpdated = response.data[0].collected_dt
+                this.textTimeAgo = response.data[0].time_ago
+                })
+    },
+    computed:{
+        justTime: function(){
+            let time = new Date(this.lastUpdated)
+            return time.toLocaleTimeString()
+        }
+    }
+
+}
+</script>
 <style scope>
 .header {
     margin-top: 20px;
@@ -11,5 +42,11 @@
 .center-text {
     color: #333333;
     font-size: xx-large;
+}
+.updated-text{
+    margin:0;
+    position:relative;
+    color:#5c5858;
+    font-size: small;
 }
 </style>
